45009d180b06a875f2f11dc095598e1e872d1c36
Infi <infi@infi.sh>
2023-10-02 00:56:41 +0200

    fix: url in infosheet should correctly take up max width
    Signed-off-by: Infi <infi@infi.sh>

--- app/src/main/java/chat/revolt/sheets/LinkInfoSheet.kt
+++ app/src/main/java/chat/revolt/sheets/LinkInfoSheet.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -45,6 +46,7 @@ fun LinkInfoSheet(url: String, onDismiss: () -> Unit) {
         Box(
             modifier = Modifier
                 .clip(MaterialTheme.shapes.medium)
+                .fillMaxWidth()
                 .background(MaterialTheme.colorScheme.surfaceColorAtElevation(0.dp))
                 .clickable(onClick = {
                     if (url.startsWith("revolt-android://")) return@clickable
