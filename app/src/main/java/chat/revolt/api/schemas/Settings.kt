ac1abc749bf715863afe8d1d1c2159510ae167b5
Infi <infi@infi.sh>
2023-10-01 18:23:04 +0200

    docs: code-fence theme enum
    Signed-off-by: Infi <infi@infi.sh>

--- app/src/main/java/chat/revolt/api/schemas/Settings.kt
+++ app/src/main/java/chat/revolt/api/schemas/Settings.kt
@@ -11,7 +11,7 @@ data class OrderingSettings(
 data class AndroidSpecificSettings(
     /**
      * The theme to use for the app.
-     * Can be one of { None, Revolt, Light, M3Dynamic, Amoled }
+     * Can be one of `{ None, Revolt, Light, M3Dynamic, Amoled }`
      */
     var theme: String? = null,
 )
