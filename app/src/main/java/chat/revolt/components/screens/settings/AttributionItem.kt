ba40698ad4385fb14379911e1723d391c93d02a7
Infi <infi@infi.sh>
2024-01-08 01:01:49 +0100

    fix: material 3-ise attribution further
    Signed-off-by: Infi <infi@infi.sh>

--- app/src/main/java/chat/revolt/components/screens/settings/AttributionItem.kt
+++ app/src/main/java/chat/revolt/components/screens/settings/AttributionItem.kt
@@ -1,41 +1,28 @@
 package chat.revolt.components.screens.settings
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import chat.revolt.R
 import chat.revolt.screens.about.Library
 
 @Composable
 fun AttributionItem(library: Library, onClick: () -> Unit) {
-    SelectionContainer {
-        Column(
-            modifier = Modifier
-                .clickable(onClick = onClick)
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
+    ListItem(
+        headlineContent = {
             Text(
-                text = library.name,
-                style = MaterialTheme.typography.bodyLarge
+                text = library.name
             )
-
-            Spacer(modifier = Modifier.height(8.dp))
-
+        },
+        supportingContent = {
             Text(
-                text = stringResource(id = R.string.oss_attribution_tap_to_view_license),
-                style = MaterialTheme.typography.bodySmall
+                text = stringResource(id = R.string.oss_attribution_tap_to_view_license)
             )
-        }
-    }
+        },
+        modifier = Modifier
+            .clickable(onClick = onClick)
+    )
 }
