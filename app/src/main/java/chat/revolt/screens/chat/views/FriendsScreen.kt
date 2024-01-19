984ee0c2fcc7a3fd16e4dc3c775cc2819efea8c7
Infi <infi@infi.sh>
2024-01-19 23:51:46 +0100

    fix: issue where FriendsScreen is not full height
    Signed-off-by: Infi <infi@infi.sh>

--- app/src/main/java/chat/revolt/screens/chat/views/FriendsScreen.kt
+++ app/src/main/java/chat/revolt/screens/chat/views/FriendsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -101,7 +102,10 @@ fun FriendsScreen(useDrawer: Boolean, onDrawerClicked: () -> Unit) {
         }
     }
 
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+    ) {
         PageHeader(
             text = stringResource(R.string.friends),
             startButtons = {
