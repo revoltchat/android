546dd7bdebc6cf16a67edfadac77a3fcaf7323ce
Infi <infi@infi.sh>
2024-06-03 20:03:51 +0200

    chore: change share target activity design to reflect cs2
    Signed-off-by: Infi <infi@infi.sh>

--- app/src/main/java/chat/revolt/activities/ShareTargetActivity.kt
+++ app/src/main/java/chat/revolt/activities/ShareTargetActivity.kt
@@ -43,7 +43,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -382,16 +381,7 @@ fun ShareTargetScreen(
                         }
                     }
 
-                    Column(
-                        modifier = Modifier
-                            .padding(
-                                start = 16.dp,
-                                end = 16.dp,
-                                bottom = 16.dp,
-                                top = 8.dp
-                            )
-                            .clip(MaterialTheme.shapes.medium)
-                    ) {
+                    Column {
                         AnimatedVisibility(viewModel.attachments.isNotEmpty()) {
                             AttachmentManager(
                                 attachments = viewModel.attachments,
