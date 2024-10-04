48e20d54641bbffe5c8ae1e2952e899e21037838
Infi <infi@infi.sh>
2024-10-04 19:19:51 +0200

    feat: special embeds control UI
    Signed-off-by: Infi <infi@infi.sh>

--- app/src/main/java/chat/revolt/components/chat/specialembeds/SpecialEmbedSwitch.kt
+++ app/src/main/java/chat/revolt/components/chat/specialembeds/SpecialEmbedSwitch.kt
@@ -3,12 +3,21 @@ package chat.revolt.components.chat.specialembeds
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import chat.revolt.api.schemas.Special
+import chat.revolt.api.settings.LoadedSettings
 
 @Composable
 fun SpecialEmbedSwitch(special: Special, modifier: Modifier = Modifier) {
-    when (special.type) {
-        "YouTube" -> YoutubeEmbedSwitch(special, modifier)
-        "AppleMusic" -> AppleMusicEmbed(special, modifier)
+    when {
+        (special.type == "YouTube") && LoadedSettings.specialEmbedSettings.embedYouTube -> YoutubeEmbedSwitch(
+            special,
+            modifier
+        )
+
+        (special.type == "AppleMusic") && LoadedSettings.specialEmbedSettings.embedAppleMusic -> AppleMusicEmbed(
+            special,
+            modifier
+        )
+
         else -> {}
     }
 }
--- app/src/main/java/chat/revolt/screens/settings/ChatSettingsScreen.kt
+++ app/src/main/java/chat/revolt/screens/settings/ChatSettingsScreen.kt
@@ -1,5 +1,6 @@
 package chat.revolt.screens.settings
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
@@ -13,7 +14,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -21,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -28,6 +32,7 @@ import androidx.navigation.NavController
 import chat.revolt.R
 import chat.revolt.api.settings.LoadedSettings
 import chat.revolt.api.settings.MessageReplyStyle
+import chat.revolt.api.settings.SpecialEmbedSettings
 import chat.revolt.api.settings.SyncedSettings
 import chat.revolt.components.generic.ListHeader
 import chat.revolt.components.generic.RadioItem
@@ -38,6 +43,13 @@ class ChatSettingsScreenViewModel : ViewModel() {
         viewModelScope.launch {
             SyncedSettings.updateAndroid(SyncedSettings.android.copy(messageReplyStyle = next.name))
             LoadedSettings.messageReplyStyle = next
+        }
+    }
+
+    fun updateSpecialEmbedSettings(next: SpecialEmbedSettings) {
+        viewModelScope.launch {
+            SyncedSettings.updateAndroid(SyncedSettings.android.copy(specialEmbedSettings = next))
+            LoadedSettings.specialEmbedSettings = next
         }
     }
 }
@@ -104,6 +116,58 @@ fun ChatSettingsScreen(
                     selected = LoadedSettings.messageReplyStyle == MessageReplyStyle.DoubleTap,
                     onClick = { viewModel.updateMessageReplyStyle(MessageReplyStyle.DoubleTap) },
                     label = { Text(text = stringResource(R.string.settings_chat_quick_reply_double_tap)) }
+                )
+            }
+
+            ListHeader {
+                Text(
+                    text = stringResource(R.string.settings_chat_interactive_embeds)
+                )
+            }
+
+            Text(
+                stringResource(R.string.settings_chat_interactive_embeds_description),
+                modifier = Modifier
+                    .padding(vertical = 8.dp, horizontal = 16.dp),
+            )
+
+            Column {
+                ListItem(
+                    headlineContent = {
+                        Text(stringResource(R.string.settings_chat_interactive_embeds_youtube))
+                    },
+                    trailingContent = {
+                        Switch(
+                            checked = LoadedSettings.specialEmbedSettings.embedYouTube,
+                            onCheckedChange = null
+                        )
+                    },
+                    modifier = Modifier.clickable {
+                        viewModel.updateSpecialEmbedSettings(
+                            LoadedSettings.specialEmbedSettings.copy(
+                                embedYouTube = !LoadedSettings.specialEmbedSettings.embedYouTube
+                            )
+                        )
+                    }
+                )
+
+                ListItem(
+                    headlineContent = {
+                        Text(stringResource(R.string.settings_chat_interactive_embeds_apple_music))
+                    },
+                    trailingContent = {
+                        Switch(
+                            checked = LoadedSettings.specialEmbedSettings.embedAppleMusic,
+                            onCheckedChange = null
+                        )
+                    },
+                    modifier = Modifier.clickable {
+                        viewModel.updateSpecialEmbedSettings(
+                            LoadedSettings.specialEmbedSettings.copy(
+                                embedAppleMusic = !LoadedSettings.specialEmbedSettings.embedAppleMusic
+                            )
+                        )
+                    }
                 )
             }
         }
