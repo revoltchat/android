eeaf06d4ba0e83cf671e225c3d8728a217f63160
Infi <infi@infi.sh>
2024-10-04 19:03:50 +0200

    fix: do not deform role colour indicator in any circumstance
    Signed-off-by: Infi <infi@infi.sh>

--- app/src/main/java/chat/revolt/components/chat/RoleListEntry.kt
+++ app/src/main/java/chat/revolt/components/chat/RoleListEntry.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,7 +32,7 @@ fun RoleListEntry(label: String, brush: Brush, modifier: Modifier = Modifier) {
         Box(
             modifier = Modifier
                 .clip(CircleShape)
-                .size(14.dp)
+                .requiredSize(14.dp)
                 .background(brush = brush)
         )
     }
